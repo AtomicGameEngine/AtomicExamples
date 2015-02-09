e554a840a6f8df694d38ea562863f77a596f41fb
Josh Engebretson <josh@galaxyfarfaraway.com>
2015-02-08 16:39:28 -0800

    More new platformer updates

---
+++ PhysicsPlatformerNew/Resources/Components/Lighting.js
@@ -0,0 +1,37 @@
+
+// Atomic Component
+
+var game = Atomic.game;
+var scene = game.scene;
+var node = self.node;
+
+var daytime = true;
+
+function start() {
+
+    // currently lightgroup must be created after viewport 0 is set
+    var lightGroup = node.createComponent("Light2DGroup");
+    lightGroup.setPhysicsWorld(Platformer.physicsWorld);
+    
+    if (daytime)
+        lightGroup.ambientColor = [1, 1, 1, 1];
+    else
+        lightGroup.ambientColor = [.8, .8, .8, .25];
+    
+    
+    if (daytime)
+    {
+        TheSun = scene.createComponent("DirectionalLight2D");
+        TheSun.color = [1, 1, 1, 0.15];
+        TheSun.castShadows = true;
+        TheSun.numRays = 1024;
+        TheSun.backtrace = false;        
+        lightGroup.addLight(TheSun);
+    }
+
+
+}
+
+function update(timeStep) {
+
+}
