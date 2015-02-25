b41be3fc43e89536e58777618fa832a32f7194d2
Josh Engebretson <josh@galaxyfarfaraway.com>
2015-02-24 17:07:19 -0800

    Adding visual cue to Light2D shadow casters

--- Light2DExample/Resources/Components/ShadowCaster.js
+++ Light2DExample/Resources/Components/ShadowCaster.js
@@ -6,10 +6,18 @@ body.bodyType = Atomic.BT_STATIC;
 body.castShadows = true;
 
 var circle = node.createComponent("CollisionCircle2D");
-circle.radius = .25;
+circle.radius = .35;
+
+var sprite2D = game.cache.getResource("Sprite2D", "Sprites/ball" + (Math.round(Math.random() * 7) + 1) + ".png");
+var sprite = node.createComponent("StaticSprite2D");
+sprite.setSprite(sprite2D);
 
 var x = -Light2DExample.halfWidth + (Light2DExample.halfWidth * 2) * Math.random();
 var y = -Light2DExample.halfHeight + (Light2DExample.halfHeight * 2) * Math.random();
+
+// tolerance towards the middle of screen
+x *= .7;
+y *= .7;
 
 node.position2D = [x, y];
 
