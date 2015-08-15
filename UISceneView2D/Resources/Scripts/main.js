59ec6d3bb229797c6e780b7324ad050dc1ace345
Josh Engebretson <josh@galaxyfarfaraway.com>
2015-08-15 11:24:25 -0700

    Update UISceneView2D

--- UISceneView2D/Resources/Scripts/main.js
+++ UISceneView2D/Resources/Scripts/main.js
@@ -1,24 +1,11 @@
 // This script is the main entry point of the game
-require("AtomicGame");
 
-Atomic.game.init(start, update);
+var scene = Atomic.player.loadScene("Scenes/MainScene.scene");
 
-
-// called at the start of play
-function start() {
-
-    var game = Atomic.game;
-
-    // create a 2D scene
-    game.createScene2D();
-
-    var uiNode = game.scene.createChild("UI");
-    uiNode.createJSComponent("UI");
-
-}
-
-// called per frame
+// called per frame, optional
 function update(timeStep) {
 
 
 }
+
+exports.update = update;
