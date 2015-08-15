39d27d4b34b50b28b085d354df5c475f33864d05
Josh Engebretson <josh@galaxyfarfaraway.com>
2015-08-14 19:36:11 -0700

    Updating Basic2D

--- Basic2D/Resources/Components/Star.js
+++ Basic2D/Resources/Components/Star.js
@@ -1,16 +1,22 @@
-var game = Atomic.game;
-var node = self.node;
+'atomic component';
 
-function start() {
+exports.component = function(self) {
 
-	var sprite2D = node.createComponent("StaticSprite2D");
-	sprite2D.sprite = game.getSprite2D("Sprites/star.png");
-	sprite2D.blendMode = Atomic.BLEND_ALPHA;
-	
-}
+    var node = self.node;
 
-function update(timeStep) {	
+    self.start = function() {
 
-	node.roll(timeStep * 100);
+        var sprite2D = node.createComponent("StaticSprite2D");
+        sprite2D.sprite = Atomic.cache.getResource("Sprite2D", "Sprites/star.png");
+        sprite2D.blendMode = Atomic.BLEND_ALPHA;
+
+    }
+
+    self.update = function(timeStep) {
+
+        node.roll(timeStep * 100);
+
+    }
+
 
 }
--- Basic2D/Resources/Scripts/main.js
+++ Basic2D/Resources/Scripts/main.js
@@ -1,22 +1,8 @@
 
 // This script is the main entry point of the game
-require("AtomicGame");
 
-Atomic.game.init(start, update);
+Atomic.player.loadScene("Scenes/Scene.scene");
 
-
-// called at the start of play
-function start() {
-
-	var game = Atomic.game;
-
-	// create a 2D scene
-	game.createScene2D();
-
-    var spaceNode = game.scene.createChild("Star");
-    spaceNode.createJSComponent("Star");
-
-}
 
 // called per frame
 function update(timeStep) {
