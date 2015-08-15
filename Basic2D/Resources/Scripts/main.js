bb313097f744325423dfd9ccc0756e0434fd77bb
Josh Engebretson <josh@galaxyfarfaraway.com>
2015-08-14 19:45:40 -0700

    Use proper export

--- Basic2D/Resources/Scripts/main.js
+++ Basic2D/Resources/Scripts/main.js
@@ -1,11 +1,11 @@
-
 // This script is the main entry point of the game
 
-Atomic.player.loadScene("Scenes/Scene.scene");
+var scene = Atomic.player.loadScene("Scenes/Scene.scene");
 
-
-// called per frame
+// called per frame, optional
 function update(timeStep) {
 
 
 }
+
+exports.update = update;
