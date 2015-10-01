439cbf905218b5b0216e4ed418edaec8907e5cf0
rsredsq <rsredsq@gmail.com>
2015-10-02 01:10:26 +0300

    Small changes

--- ToonTown/Resources/Scripts/main.js
+++ ToonTown/Resources/Scripts/main.js
@@ -1,7 +1,8 @@
 // This script is the main entry point of the game
 //Load scene
-
-Atomic.renderer.reuseShadowMaps = false;
-Atomic.renderer.shadowQuality = Atomic.SHADOWQUALITY_LOW_16BIT;
+if(Atomic.platform == "Android" || Atomic.platform == "iOS") {
+  Atomic.renderer.reuseShadowMaps = false;
+  Atomic.renderer.shadowQuality = Atomic.SHADOWQUALITY_LOW_16BIT;
+}
 
 Atomic.player.loadScene("Scenes/ToonTown.scene");
