99ac8b28c235cd2b266825c4d35eb348c34f27c7
Josh Engebretson <josh@galaxyfarfaraway.com>
2015-11-25 12:29:04 -0800

    Adding Cubemap Example

---
+++ CubeMapExample/Resources/Components/Spinner.js
@@ -0,0 +1,17 @@
+
+"atomic component";
+
+var inspectorFields = {
+  speed: 1.0
+}
+
+exports.component = function(self) {
+
+  self.update = function(timeStep) {
+
+    self.node.yaw(timeStep * 75 * self.speed);
+    self.node.roll(timeStep * 25 * self.speed);
+    self.node.pitch(timeStep * 15 * self.speed);
+  }
+
+}
---
+++ CubeMapExample/Resources/Scripts/main.js
@@ -0,0 +1,10 @@
+// This script is the main entry point of the game
+
+var scene = Atomic.player.loadScene("Scenes/CubemapScene.scene");
+
+// called per frame, optional
+function update(timeStep) {
+
+}
+
+exports.update = update;
