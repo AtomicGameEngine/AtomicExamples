9f7b0eec436a5e78a5142b459d73e26594d347f9
Lara Engebretson <lara@galaxyfarfaraway.com>
2016-01-16 16:26:27 -0800

    Adding the WebView3D example with 3D scene

---
+++ WebView3D/Resources/Components/LightFlicker.js
@@ -0,0 +1,38 @@
+"atomic component"
+
+// a flickering light component
+exports.component = function(self){
+  var node = self.node;
+  self.light = node.getComponent("Light");
+  var baseRange = 45;
+  var targetValue = baseRange;
+
+  //define a flicker pattern
+  var flicker = "mmmaaaammmaaaabcdefgabcdefg";
+  var index = Math.random() * (flicker.length - 1);
+
+  // make sure first update catches
+  var time = 100;
+
+  self.update = function(timestep) {
+
+    time += timestep;
+    if (time > .05)
+    {
+      index++;
+      time = 0.0;
+      if (index >= flicker.length)
+        index = 0;
+
+      targetValue = baseRange * (flicker.charCodeAt(index)/255);
+
+    }
+
+    if (self.light.range < targetValue)
+      self.light.range += timestep * 10;
+
+    if (self.light.range > targetValue)
+      self.light.range -= timestep * 10;
+
+  }
+}
---
+++ WebView3D/Resources/Components/Scene.js
@@ -0,0 +1,22 @@
+"atomic component"
+
+//Define a Scene component
+exports.component = function(self) {
+  //we are attaching that component to the Scene, so we are sure that ours node is a scene
+  var scene = self.node;
+  var time = 12;
+
+  self.start = function() {
+
+      // create the procedural sky
+      var pnode = scene.createChild();
+      self.procSky = pnode.createComponent("ProcSky");
+      self.procSky.setDayTime(time);
+  }
+
+  self.update = function(timeStep) {
+
+      time += timeStep * .08;
+      self.procSky.setDayTime(time);
+  }
+}
