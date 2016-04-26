60e7d8a56a22bcac24a454f482a99a0f1fdbf843
Shaddock Heath <shaddockh@gmail.com>
2016-04-25 19:30:58 -0500

    merge from upstream

--- EditorPluginExample/Resources/EditorData/TSExample.plugin.js
+++ EditorPluginExample/Resources/EditorData/TSExample.plugin.js
@@ -1,17 +1,16 @@
-"use strict";
-var ExamplePluginUILabel = "TS Example Plugin";
+var ExamplePluginUILabel = "Example Plugin";
 var ExamplePluginTBPath = "EditorData/Example.tb.txt";
-var TSExamplePluginService = (function () {
-    function TSExamplePluginService() {
+var ExamplePluginService = (function () {
+    function ExamplePluginService() {
         var _this = this;
-        this.name = "TSExampleService";
-        this.description = "This service demonstrates plugin functionality written in TypeScript.";
+        this.name = "ExampleService";
+        this.description = "This service demonstrates plugin functionality functionality.";
         this.serviceLocator = null;
         this.extensionWindow = null;
+        this.lastObjectName = null;
         this.handleWidgetEvent = function (ev) {
-            if (!_this.extensionWindow) {
+            if (!_this.extensionWindow)
                 return;
-            }
             if (ev.type == Atomic.UI_EVENT_TYPE_CLICK) {
                 if (ev.target.id == "example_cancel") {
                     _this.extensionWindow.hide();
@@ -26,7 +25,7 @@ var TSExamplePluginService = (function () {
             return false;
         };
     }
-    TSExamplePluginService.prototype.initialize = function (serviceLoader) {
+    ExamplePluginService.prototype.initialize = function (serviceLoader) {
         Atomic.print("ExamplePluginService.initialize");
         this.serviceLocator = (serviceLoader);
         if (this.serviceLocator) {
@@ -34,7 +33,9 @@ var TSExamplePluginService = (function () {
             this.serviceLocator.uiServices.register(this);
         }
     };
-    TSExamplePluginService.prototype.projectUnloaded = function () {
+    ExamplePluginService.prototype.projectUnloaded = function () {
+        this.serviceLocator.uiServices.removeProjectContextMenuItemSource(ExamplePluginUILabel);
+        this.serviceLocator.uiServices.removeHierarchyContextMenuItemSource(ExamplePluginUILabel);
         this.serviceLocator.uiServices.removePluginMenuItemSource(ExamplePluginUILabel);
         Atomic.print("ExamplePluginService.projectUnloaded");
         if (this.serviceLocator) {
@@ -42,31 +43,52 @@ var TSExamplePluginService = (function () {
             this.serviceLocator.uiServices.unregister(this);
         }
     };
-    TSExamplePluginService.prototype.projectLoaded = function (ev) {
+    ExamplePluginService.prototype.projectLoaded = function (ev) {
         Atomic.print("ExamplePluginService.projectLoaded");
-        var menu = this.serviceLocator.uiServices.createPluginMenuItemSource(ExamplePluginUILabel, { "Open": ["tsexampleplugin open"] });
+        this.serviceLocator.uiServices.createPluginMenuItemSource(ExamplePluginUILabel, { "Open": ["exampleplugin open"] });
+        this.serviceLocator.uiServices.createHierarchyContextMenuItemSource(ExamplePluginUILabel, { "Get name": ["exampleplugin hierarchy context"] });
+        this.serviceLocator.uiServices.createProjectContextMenuItemSource(ExamplePluginUILabel, { "Get name": ["exampleplugin project context"] });
     };
-    TSExamplePluginService.prototype.playerStarted = function () {
+    ExamplePluginService.prototype.playerStarted = function () {
         Atomic.print("ExamplePluginService.playerStarted");
     };
-    TSExamplePluginService.prototype.menuItemClicked = function (refId) {
+    ExamplePluginService.prototype.menuItemClicked = function (refId) {
         Atomic.print("ExamplePluginService.menuItemClicked: " + refId);
-        if (refId == "tsexampleplugin open") {
+        if (refId == "exampleplugin open") {
             this.extensionWindow = this.serviceLocator.uiServices.showModalWindow(ExamplePluginUILabel, ExamplePluginTBPath, this.handleWidgetEvent);
             this.getWidgets();
             return true;
         }
         return false;
     };
-    TSExamplePluginService.prototype.getWidgets = function () {
-        if (!this.extensionWindow) {
-            return;
+    ExamplePluginService.prototype.hierarchyContextItemClicked = function (node, refid) {
+        Atomic.print("ExamplePluginService.hierarchyContextItemClicked: " + node.name + " " + refid);
+        if (refid == "exampleplugin hierarchy context") {
+            this.lastObjectName = "node " + node.name;
+            return true;
         }
+        return false;
+    };
+    ExamplePluginService.prototype.projectContextItemClicked = function (asset, refid) {
+        Atomic.print("ExamplePluginService.projectContextItemClicked: " + asset.name + " " + refid);
+        if (refid == "exampleplugin project context") {
+            this.lastObjectName = "asset " + asset.name;
+            return true;
+        }
+        return false;
+    };
+    ExamplePluginService.prototype.getWidgets = function () {
+        if (!this.extensionWindow)
+            return;
         this.helloLabel = this.extensionWindow.getWidget("example_hello");
         this.nameField = this.extensionWindow.getWidget("example_name");
+        if (this.lastObjectName) {
+            this.nameField.text = this.lastObjectName;
+            this.lastObjectName = null;
+        }
     };
-    return TSExamplePluginService;
-}());
-var examplePluginService = new TSExamplePluginService();
+    return ExamplePluginService;
+})();
+var examplePluginService = new ExamplePluginService();
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.default = examplePluginService;
--- EditorPluginExample/Resources/EditorData/TSExample.plugin.ts
+++ EditorPluginExample/Resources/EditorData/TSExample.plugin.ts
@@ -1,19 +1,21 @@
 /// <reference path="../../typings/Atomic/AtomicWork.d.ts" />
 /// <reference path="../../typings/Atomic/EditorWork.d.ts" />
 
-const ExamplePluginUILabel = "TS Example Plugin";
+const ExamplePluginUILabel = "Example Plugin";
 const ExamplePluginTBPath = "EditorData/Example.tb.txt";
 
-class TSExamplePluginService implements Editor.HostExtensions.HostEditorService, Editor.HostExtensions.ProjectServicesEventListener, Editor.HostExtensions.UIServicesEventListener {
+class ExamplePluginService implements Editor.HostExtensions.HostEditorService, Editor.HostExtensions.ProjectServicesEventListener, Editor.HostExtensions.UIServicesEventListener {
 
-    name: string = "TSExampleService";
-    description: string = "This service demonstrates plugin functionality written in TypeScript.";
+    name: string = "ExampleService";
+    description: string = "This service demonstrates plugin functionality functionality.";
 
     private serviceLocator: Editor.HostExtensions.HostServiceLocator = null;
     private extensionWindow: Editor.Modal.ExtensionWindow = null;
 
     private helloLabel: Atomic.UITextField;
     private nameField: Atomic.UIEditField;
+
+    private lastObjectName: string = null;
 
     initialize(serviceLoader: Editor.HostExtensions.HostServiceLocator) {
         Atomic.print("ExamplePluginService.initialize");
@@ -25,6 +27,8 @@ class TSExamplePluginService implements Editor.HostExtensions.HostEditorService,
         }
     }
     projectUnloaded() {
+        this.serviceLocator.uiServices.removeProjectContextMenuItemSource(ExamplePluginUILabel);
+        this.serviceLocator.uiServices.removeHierarchyContextMenuItemSource(ExamplePluginUILabel);
         this.serviceLocator.uiServices.removePluginMenuItemSource(ExamplePluginUILabel);
 
         Atomic.print("ExamplePluginService.projectUnloaded");
@@ -35,7 +39,9 @@ class TSExamplePluginService implements Editor.HostExtensions.HostEditorService,
     }
     projectLoaded(ev: Editor.EditorEvents.LoadProjectEvent) {
         Atomic.print("ExamplePluginService.projectLoaded");
-        let menu = this.serviceLocator.uiServices.createPluginMenuItemSource(ExamplePluginUILabel, { "Open" : ["tsexampleplugin open"] });
+        this.serviceLocator.uiServices.createPluginMenuItemSource(ExamplePluginUILabel, { "Open" : ["exampleplugin open"] });
+        this.serviceLocator.uiServices.createHierarchyContextMenuItemSource(ExamplePluginUILabel, { "Get name" : ["exampleplugin hierarchy context"]});
+        this.serviceLocator.uiServices.createProjectContextMenuItemSource(ExamplePluginUILabel, { "Get name" : ["exampleplugin project context"]});
     }
     playerStarted() {
         Atomic.print("ExamplePluginService.playerStarted");
@@ -43,8 +49,8 @@ class TSExamplePluginService implements Editor.HostExtensions.HostEditorService,
 
     menuItemClicked(refId: string): boolean {
         Atomic.print("ExamplePluginService.menuItemClicked: " + refId);
-
-        if (refId == "tsexampleplugin open") {
+        
+        if (refId == "exampleplugin open") {
             this.extensionWindow = this.serviceLocator.uiServices.showModalWindow(
                 ExamplePluginUILabel, ExamplePluginTBPath, this.handleWidgetEvent);
             this.getWidgets();
@@ -54,19 +60,46 @@ class TSExamplePluginService implements Editor.HostExtensions.HostEditorService,
         return false;
     }
 
-    getWidgets() {
-        if (!this.extensionWindow) {
-            return;
+    hierarchyContextItemClicked(node: Atomic.Node, refid: string): boolean {
+        Atomic.print("ExamplePluginService.hierarchyContextItemClicked: " + node.name + " " + refid);
+
+        if (refid == "exampleplugin hierarchy context") {
+            this.lastObjectName = "node " + node.name;
+
+            return true;
         }
+
+        return false;
+    }
+
+    projectContextItemClicked(asset: ToolCore.Asset, refid: string): boolean {
+        Atomic.print("ExamplePluginService.projectContextItemClicked: " + asset.name + " " + refid);
+
+        if (refid == "exampleplugin project context") {
+            this.lastObjectName = "asset " + asset.name;
+
+            return true;
+        }
+
+        return false;
+    }
+
+    getWidgets() {
+        if (!this.extensionWindow)
+            return;
 
         this.helloLabel = <Atomic.UITextField>this.extensionWindow.getWidget("example_hello");
         this.nameField = <Atomic.UIEditField>this.extensionWindow.getWidget("example_name");
+
+        if (this.lastObjectName) {
+            this.nameField.text = this.lastObjectName;
+            this.lastObjectName = null;
+        }
     }
 
     handleWidgetEvent = (ev: Atomic.UIWidgetEvent): boolean => { // => notation used to bind "this" to the method
-        if (!this.extensionWindow) {
+        if (!this.extensionWindow)
             return;
-        }
 
         if (ev.type == Atomic.UI_EVENT_TYPE_CLICK) {
 
@@ -86,8 +119,8 @@ class TSExamplePluginService implements Editor.HostExtensions.HostEditorService,
 
         return false;
 
-    };
+    }
 }
 
-const examplePluginService = new TSExamplePluginService();
+const examplePluginService = new ExamplePluginService();
 export default examplePluginService;
