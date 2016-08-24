c1139743314dfee3e5170ebb49b396de6d8cdf46
Josh Engebretson <josh@galaxyfarfaraway.com>
2016-08-24 14:49:30 -0700

    Adding Atomic Mutant C# scample

--- AtomicNET/AtomicMutant/Resources/Scripts/Program.cs
+++ AtomicNET/AtomicMutant/Resources/Scripts/Program.cs
@@ -7,10 +7,8 @@ public static class AtomicMain
     /// The main entry point for the application.
     /// </summary>
     static void Main(string[] args)
-    {
-
-        AtomicNET.GetSubsystem<Player>().LoadScene("Scenes/Scene.scene");
-
+    {        
+        var scene = AtomicNET.GetSubsystem<Player>().LoadScene("Scenes/Scene.scene");
     }
 
 }
